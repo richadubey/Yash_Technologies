31688a6b8c573a46d10b6d4671d66556fd1d57bb
Richa Dubey <richadubey39@gmail.com>
2024-03-29 16:20:13 +0530

    Add changes for user create and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,37 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:update]
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      notify_third_party_apis(@user)
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @user.update(user_params)
+      notify_third_party_apis(@user)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def notify_third_party_apis(user_data_change)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/create'
-  get 'users/update'
+  # post 'users/create'
+  # put 'users/update/:id'
+  resources :users, only: [:create, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
